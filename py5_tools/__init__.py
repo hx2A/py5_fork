5ad7b15865c50183becc9a2ed74aff8ed3b740de
Jim <james.schmitz@gmail.com>
2023-07-07 23:30:48 -0400

    code changes for fix

--- py5_tools/__init__.py
+++ py5_tools/__init__.py
@@ -28,4 +28,4 @@ from .config import *  # noqa
 from . import translators  # noqa
 
 
-__version__ = '0.9.1.rc1'
+__version__ = '0.9.1.rc2'
--- py5_tools/imported.py
+++ py5_tools/imported.py
@@ -104,9 +104,9 @@ if {1} and is_dead_from_error:
 """
 
 
-SETTINGS_REGEX = re.compile(r'^def settings\(\):', flags=re.MULTILINE)
-SETUP_REGEX = re.compile(r'^def setup\(\):', flags=re.MULTILINE)
-DRAW_REGEX = re.compile(r'^def draw\(\):', flags=re.MULTILINE)
+SETTINGS_REGEX = re.compile(r'^def settings[^:]*:', flags=re.MULTILINE)
+SETUP_REGEX = re.compile(r'^def setup[^:]*:', flags=re.MULTILINE)
+DRAW_REGEX = re.compile(r'^def draw[^:]*:', flags=re.MULTILINE)
 
 
 def is_static_mode(code):
